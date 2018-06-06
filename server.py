fc9e9c0f48bb7fa9ba90201e59f6537827a4e5c0
Skander <skander.kc@gmail.com>
2018-06-05 21:50:24 -0400

    Create principal routes

--- server.py
+++ server.py
@@ -3,6 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-  print('yo')
-  return render_template('hello.html')
+def index():
+  return render_template('index.html')
+
+@app.route('/demande')
+def demande():
+  return render_template('demande.html')
+
+@app.route('/photos')
+def photos():
+  return render_template('photos.html')
